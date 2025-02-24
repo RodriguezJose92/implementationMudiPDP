952deecc9de699cb933e96fb182c479b19a5bf96
Jose Rodriguez <131327988+RodriguezJose92@users.noreply.github.com>
2025-02-24 15:23:59 -0500

    Update ModuleMudi.js

--- ModuleMudi.js
+++ ModuleMudi.js
@@ -100,28 +100,28 @@ class ModalMudi {
   
                 <div class="titleContainer">
                     <div class="iconTitle">
-                        <img class="stepMudi step1" src="https://cdn.jsdelivr.net/gh/RodriguezJose92/implementationMudiPDP@latest/assets/step3amoblando.webp">
+                        <img class="stepMudi step1" src="https://cdn.jsdelivr.net/gh/RodriguezJose92/implementationMudiPDP@latest/assets/step3.webp">
                     </div>
                     <p class="textInfoMudi">Apunta el teléfono al piso.</p>
                 </div>
   
                 <div class="titleContainer">
                     <div class="iconTitle">
-                        <img class="stepMudi step2" src="https://cdn.jsdelivr.net/gh/RodriguezJose92/implementationMudiPDP@latest/assets/step4amoblando.webp">
+                        <img class="stepMudi step2" src="https://cdn.jsdelivr.net/gh/RodriguezJose92/implementationMudiPDP@latest/assets/step4.webp">
                     </div>
                     <p class="textInfoMudi">Desplaza para visualizar.</p>
                 </div>
   
                 <div class="titleContainer">
                     <div class="iconTitle">
-                        <img class="stepMudi step3" src="https://cdn.jsdelivr.net/gh/RodriguezJose92/implementationMudiPDP@latest/assets/step2amoblando.webp">
+                        <img class="stepMudi step3" src="https://cdn.jsdelivr.net/gh/RodriguezJose92/implementationMudiPDP@latest/assets/step2.webp">
                     </div>
                     <p class="textInfoMudi">Amplia y detalla el producto.</p>
                 </div>
   
                 <div class="titleContainer">
                     <div class="iconTitle">
-                        <img class="stepMudi step4" src="https://cdn.jsdelivr.net/gh/RodriguezJose92/implementationMudiPDP@latest/assets/step1amoblando.webp">
+                        <img class="stepMudi step4" src="https://cdn.jsdelivr.net/gh/RodriguezJose92/implementationMudiPDP@latest/assets/step1.webp">
                     </div>
                     <p class="textInfoMudi">Toca dos veces para restablecer.</p>
                 </div>
